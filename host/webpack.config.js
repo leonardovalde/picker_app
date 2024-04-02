bc79860bedbbfe497996e239fafc9e1b23dc6b72
leonardovalde <leonardovalde16@gmail.com>
2024-04-02 10:30:44 -0500

    fix link

--- host/webpack.config.js
+++ host/webpack.config.js
@@ -4,7 +4,7 @@ const Dotenv = require('dotenv-webpack')
 const deps = require('./package.json').dependencies
 module.exports = (_, argv) => ({
   output: {
-    publicPath: 'http://localhost:3000/',
+    publicPath: 'https://picker-app-host.onrender.com/',
   },
 
   resolve: {
